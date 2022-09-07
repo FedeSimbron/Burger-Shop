64738e3946e128ea05e9d7102b9d0cbd7a7dab0c
fedesimbron <fedesimbron8@gmail.com>
2022-09-07 01:00:31 -0300

    commit-7-9-2022-01-00

--- js/inicio.js
+++ js/inicio.js
@@ -1,29 +1,46 @@
 let articulos= [{
     img:"/img/1.jpg",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-1",
     precio: "$100"
 },{
     img:"/img/2.webp",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-2",
     precio: "$200"
 },{
     img:"/img/3.jpg",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-3",
     precio: "$300"
 },{
     img:"/img/4.jpg",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-4",
     precio: "$400"
 },{
     img:"/img/5.jfif",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-5",
     precio: "$500"
 },{
     img:"/img/6.webp",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-5",
     precio: "$500"
 },{
     img:"/img/7.jfif",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
+    nombre: "hamburgesa-5",
+    precio: "$500"
+},{
+    img:"/img/7.jfif",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
+    nombre: "hamburgesa-5",
+    precio: "$500"
+},{
+    img:"/img/7.jfif",
+    iconStar:'<i class="fa-solid fa-cart-shopping"></i>',
     nombre: "hamburgesa-5",
     precio: "$500"
 }]
@@ -31,11 +48,13 @@ let articulos= [{
 for (let articulo in articulos){
     let datos = articulos[articulo];
     let img = datos["img"]
+    let iconStar = datos["iconStar"]
     let nombre = datos["nombre"];
     let precio = datos["precio"]
     let htmlCode=`
     <article>
         <div class="container-img"><img class="img-prod" src="${img}"></div> 
+        <div class="icon-star">${iconStar}</div>
         <div class="nombre-prod"><h3>${nombre}</h3></div>
         <div class="precio-prod"><h3>${precio}</h3></div>
     </article>`
