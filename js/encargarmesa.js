58ef48559c65786ef95633b7c4a1a7a80451fd97
fedesimbron <fedesimbron8@gmail.com>
2022-09-08 00:31:13 -0300

    commit-00-31

--- js/encargarmesa.js
+++ js/encargarmesa.js
@@ -1,1 +1,17 @@
-let mesas = [];
+const cantidad =document.querySelector(".cantidad")
+const personas = document.querySelector(".personas")
+const horario = document.querySelector(".horario")
+const enviar = document.querySelector(".enviar-mesas")
+const ret = document.querySelector(".hola")
+
+let mesas = 0;
+
+cantidad .innerHTML = mesas
+
+enviar.addEventListener("click",agregar)
+
+
+
+
+
+
